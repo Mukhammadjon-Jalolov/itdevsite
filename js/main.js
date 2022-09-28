7fa7e2bcd392fa29f59aee019a6bd43855b5901b
Muhammadjon <mjalolov1305@gmail.com>
2022-09-28 10:56:23 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+
+
+
+
+window.onscroll = function(){scrollMenu()}
+
+function scrollMenu(){
+    let topMenu = document.getElementById("topMenu");
+
+    if(document.body.scrollTop > 100 || document.documentElement.scrollTop > 100){
+        topMenu.style.position = "sticky";
+    } else {
+        topMenu.style.position = "relative";
+    }
+}
